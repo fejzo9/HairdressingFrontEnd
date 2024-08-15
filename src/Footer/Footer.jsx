53481dacea710af5e93b69e0e3ceb0932105b71a
fejzo9 <fejzo999@gmail.com>
2024-08-15 15:36:50 +0200

    Made footer, but not satisfiet with it. It is always on the bottom of the screen, but not on the bottom of the page

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FaFacebookF, FaInstagram, FaEnvelope } from 'react-icons/fa';
+import './Footer.css'; // Stilizujte footer prema vašim potrebama
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="social-media">
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <FaFacebookF />
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <FaInstagram />
+          </a>
+        </div>
+        <div className="contact">
+          <a href="mailto:contact@myemail.com">
+            <FaEnvelope /> contact@myemail.com
+          </a>
+        </div>
+      </div>
+      <p className="footer-bottom">© {new Date().getFullYear()} Hairdressing salons. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
