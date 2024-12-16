2f90388c8c98ec2f9ed32b315f474e38cc6868f1
fejzo9 <fejzo999@gmail.com>
2024-12-16 15:09:55 +0100

    Inside src/Maps folder added FaeSalonMap.jsx and FakeSalonMap.css that only contain the picture of the map of salons, and not the real map of the salons, because real map using Google Maps API neds first to have Billing Account, and I didn't want to do that in this moment, so I'm saving that for the end

---
+++ src/Maps/FakeSalonMap.jsx
@@ -0,0 +1,14 @@
+import "./FakeSalonMap.css";
+import Header from "../Header/Header.jsx";
+
+function FakeSalonMap(){
+    return(
+        <div className="mapa">
+    
+            <h2>Hairdressing Salon Maps</h2>
+            <img src="../../public/maps2.png" alt="Maps of Salon" style={{ width: "100%", height: "auto" }}/>
+        </div>
+    );
+}
+
+export default FakeSalonMap;
