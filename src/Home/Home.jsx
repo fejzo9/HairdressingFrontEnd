6924af8166a42f6a9b3888dff080a27f00e3c133
fejzo9 <fejzo999@gmail.com>
2024-12-17 14:54:42 +0100

    Inside Home Page, added component of Fake Map

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,12 @@
 import "./Home.css";
 import Header from "../Header/Header.jsx";
+import FakeMap from "../Maps/FakeSalonMap.jsx";
 
 function Home() {
   return (
     <div className="home">
       <Header />
+      <FakeMap />
     </div>
   );
 }
