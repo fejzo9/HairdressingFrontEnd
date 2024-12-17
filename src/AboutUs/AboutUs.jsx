ba0a946c3f582474134176071df175c5b3dac43a
fejzo9 <fejzo999@gmail.com>
2024-12-17 09:40:58 +0100

    In AboutUs.jsx added new div and class Names for styling the AboutUs page.

--- src/AboutUs/AboutUs.jsx
+++ src/AboutUs/AboutUs.jsx
@@ -23,31 +23,44 @@ function AboutUs() {
      </div>
 
       <div className="about-mission">
-        <h2>Naša Misija</h2>
-        <p>
-          Cilj nam je da pružimo kvalitetne usluge, olakšamo rezervacije i 
-          omogućimo korisnicima brzi pristup najboljim salonima u gradu.
-        </p>
-        <h3>Šta radimo?</h3>
-        <p>Pojednostavljujemo proces rezervacije termina</p>
-        <p>Pomažemo salonima u upravljanju terminima</p>
+            <h2>Naša Misija</h2>
+            <p>
+            Cilj nam je da pružimo kvalitetne usluge, olakšamo rezervacije i 
+            omogućimo korisnicima brzi pristup najboljim salonima u gradu.
+            </p>
 
-        <h3>Za koga radimo?</h3>
-        <p>Za korisnike koji žele lako pronaći slobodan termin</p>
-        <p>Za frizerske salone koji žele bolju organizaciju</p>
+            <div className="mission-items-container">
+                <div className="mission-item">
+                    <h3>Šta radimo?</h3>
+                    <p>- Pojednostavljujemo proces rezervacije termina</p>
+                    <p>- Pomažemo salonima u upravljanju terminima</p>
+                    <img src="/sta-radimo-icon.png" alt="Doing" width={180} height={180} />
+                </div>
 
-        <h3>Koje vrijednosti nudimo?</h3>
-        <p>Štedimo korisnicima vrijeme i trud</p>
-        <p>Unapređujemo poslovanje frizerskih salona</p>
+                <div className="mission-item">
+                    <h3>Za koga radimo?</h3>
+                    <p>- Za korisnike koji žele lako pronaći slobodan termin</p>
+                    <p>- Za frizerske salone koji žele bolju organizaciju</p>
+                    <img src="/za-koga-radimo-icon.png" alt="Doing" width={180} height={180} />
+                </div>
 
-        <h3>Ciljevi</h3>
-        <ul>
-            <li>Pojednostaviti zakazivanje termina u frizerskim salonima kroz modernu i intuitivnu aplikaciju koja povezuje korisnike i frizerske salone, štedi vrijeme i unapređuje korisničko iskustvo</li>
-            <li>Omogućiti korisnicima brz i jednostavan način zakazivanja frizerskih usluga, pružajući im slobodu i kontrolu nad njihovim vremenom</li>
-            <li>Pružiti frizerskim salonima alat za unapređenje poslovanja kroz bolju organizaciju i optimizaciju rezervacija</li>
-            <li>Stvoriti most između korisnika i frizerskih salona, čineći zakazivanje jednostavnijim za korisnike i efikasnijim za salone</li>
-        </ul>
-      </div>
+                <div className="mission-item">
+                    <h3>Koje vrijednosti nudimo?</h3>
+                    <p>- Štedimo korisnicima vrijeme i trud</p>
+                    <p>- Unapređujemo poslovanje frizerskih salona</p>
+                    <img src="/values-icon.png" alt="Doing" width={180} height={180} />
+                </div>
+            </div>
+                <div className="mission-item-goals">
+                    <h3>Ciljevi</h3>
+                    <ul>
+                        <li>Pojednostaviti zakazivanje termina u frizerskim salonima kroz modernu i intuitivnu aplikaciju koja povezuje korisnike i frizerske salone, štedi vrijeme i unapređuje korisničko iskustvo</li>
+                        <li>Omogućiti korisnicima brz i jednostavan način zakazivanja frizerskih usluga, pružajući im slobodu i kontrolu nad njihovim vremenom</li>
+                        <li>Pružiti frizerskim salonima alat za unapređenje poslovanja kroz bolju organizaciju i optimizaciju rezervacija</li>
+                        <li>Stvoriti most između korisnika i frizerskih salona, čineći zakazivanje jednostavnijim za korisnike i efikasnijim za salone</li>
+                    </ul>
+                </div>
+        </div>
 
       <div className="about-story">
         <h2>Naša Priča</h2>
