b29c1fb9acd8c2a60a0c72e09d14f167a6a1bfe0
fejzo9 <fejzo999@gmail.com>
2025-02-03 10:28:40 +0100

    UserTable files organized inside UserTable folder

---
+++ src/Admin/UserTable/UserTable.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import './UserTable.css';
+
+function UserTable({ users, setUsers }) {
+
+  const handleDelete = async (id) => {
+    try {
+      await fetch(`http://localhost:8080/users/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Authorization": `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      setUsers(users.filter((user) => user.id !== id));
+    } catch (error) {
+      console.error("Error deleting user:", error);
+    }
+  };
+
+  return (
+  <>
+   <div className="table-container">
+    <table className="user-table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Ime</th>
+          <th>Prezime</th>
+          <th>Datum rođenja</th>
+          <th>Spol</th>
+          <th>Email</th>
+          <th>Username</th>
+          <th>Uloga</th>
+          <th>Akcije</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.firstName}</td>
+            <td>{user.lastName}</td>
+            <td>{user.birthDate}</td>
+            <td>{user.gender}</td>
+            <td>{user.email}</td>
+            <td>{user.username}</td>
+            <td>{user.role}</td>
+            <td>
+              <button onClick={() => handleDelete(user.id)}>Delete</button>
+              <button onClick={() => alert("Uređivanje korisnika još nije implementirano!")}>Edit</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+    </div>  
+    <button onClick={() => alert("Dodavanje novog korisnika još nije implementirano!")}>
+        + Dodaj korisnika
+      </button>
+    </>
+  );
+}
+
+export default UserTable;
