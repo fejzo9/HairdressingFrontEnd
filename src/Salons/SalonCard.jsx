56ba30dcba47cd7c110d0ed80334bed5fd937f1d
fejzo9 <fejzo999@gmail.com>
2025-02-24 16:32:19 +0100

    In SalonCard transfered to parameters

--- src/Salons/SalonCard.jsx
+++ src/Salons/SalonCard.jsx
@@ -1,16 +1,19 @@
 import "./SalonCard.css";
 
-function SalonCard(props){
-
+function SalonCard({ pic, name, address, phone, email }) {
     return (
         <div className="salon-card">
-            <img className="card-image" src={props.pic} alt="slika" width={150} height={150} />
-            <h2 className="card-title">{props.name}</h2>
-            <p className="card-text">{props.address}</p>
-            <p className="card-text">{props.phone}</p>
-            <p className="card-text">{props.email}</p>
+            <img className="card-image" 
+                 src={pic || "/default-salon.png"} 
+                 alt="Salon Slika" 
+                 width={150} 
+                 height={150} />
+            <h2 className="card-title">{name}</h2>
+            <p className="card-text">{address}</p>
+            <p className="card-text">{phone}</p>
+            <p className="card-text">{email}</p>
         </div>
     );
 }
 
-export default SalonCard
+export default SalonCard;
