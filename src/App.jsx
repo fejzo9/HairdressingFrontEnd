8d4c876a4b5af98fb991b27cc0e1690f27e23e19
fejzo9 <fejzo999@gmail.com>
2025-02-25 16:13:33 +0100

    imported bootstrap in App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 import RegistrationForm from './Registration/RegistrationForm'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home/Home';
