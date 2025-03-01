c5e321b7dd07aabf2744a0c8fe09d4a93d1838c2
fejzo9 <fejzo999@gmail.com>
2025-03-01 08:10:21 +0100

    From SalonCard deleted for witdth and height properties

--- src/Salons/SalonCard.jsx
+++ src/Salons/SalonCard.jsx
@@ -6,8 +6,7 @@ function SalonCard({ pic, name, address, phone, email }) {
             <img className="card-image" 
                  src={pic || "/default-salon.png"} 
                  alt="Salon Slika" 
-                 width={150} 
-                 height={150} />
+                />
             <h2 className="card-title">{name}</h2>
             <p className="card-text">{address}</p>
             <p className="card-text">{phone}</p>
