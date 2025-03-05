300f3caf8672e2e96f8e1bf0eb025d004bf44c97
fejzo9 <fejzo999@gmail.com>
2025-03-05 05:56:29 +0100

    Adjusted AddSalon page, added bootstrap. Started implementing.

--- src/Salons/AddSalon.jsx
+++ src/Salons/AddSalon.jsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 
 function AddSalon() {
     return(
-        <div>
+        <div className="d-flex justify-content-center mt-4 bg-dark bg-opacity-50 p-4">
             <h1>Form For Adding Salon</h1>
+            
         </div>
     );
 }
