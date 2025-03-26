8954ec5f33053752e0eeed9089e8db0a39ce8789
fejzo9 <fejzo999@gmail.com>
2025-03-26 16:41:30 +0100

    Created HairdresserSchedule jsx and css file, for showing table of hairdresser.

---
+++ src/HairdresserSchedule/HairdresserSchedule.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import "./HairdresserSchedule.css";
+
+const days = ["PON", "UTO", "SRI", "CET", "PET", "SUB", "NED"];
+const startHour = 8;
+const endHour = 22;
+const timeSlots = [];
+
+for (let hour = startHour; hour < endHour; hour++) {
+    timeSlots.push(`${hour.toString().padStart(2, '0')}:00`);
+    timeSlots.push(`${hour.toString().padStart(2, '0')}:30`);
+}
+
+function HairdresserSchedule() {
+
+    const { hairdresserId } = useParams();
+
+    return (
+        <div className="container mt-4">
+            <h2 className="text-center mb-4">Frizerski Kalendar</h2>
+
+            <div className="d-flex justify-content-evenly mb-4">
+                <button className="btn btn-success me-2">Unesi sedmičnu smjenu</button>
+                <button className="btn btn-primary">Unesi smjenu po danima</button>
+            </div>
+
+            <div className="calendar-table-wrapper rounded-3 bg-light bg-opacity-25 shadow p-2">
+                <table className="table table-bordered calendar-table text-center">
+                    <thead className="table-light">
+                        <tr>
+                            <th>Vrijeme</th>
+                            {days.map(day => (
+                                <th key={day}>{day}</th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {timeSlots.map((slot, rowIndex) => (
+                            <tr key={rowIndex}>
+                                <td className="fw-bold">{slot}</td>
+                                {days.map((day, colIndex) => (
+                                    <td key={colIndex} className="time-cell">
+                                        {/* Prazna ćelija - kasnije možemo popunjavati */}
+                                    </td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
+
+export default HairdresserSchedule;
