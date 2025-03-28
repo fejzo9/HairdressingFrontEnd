41890b08e9c37d48222e8547d4f452a84d77ed64
fejzo9 <fejzo999@gmail.com>
2025-03-28 09:08:18 +0100

    Day Of Week Options created.

---
+++ src/HairdresserSchedule/utils/DayOfWeekOptions.js
@@ -0,0 +1,12 @@
+// utils/DayOfWeekOptions.js
+
+export const DayOfWeekOptions = [
+    { value: "MONDAY", label: "Ponedjeljak" },
+    { value: "TUESDAY", label: "Utorak" },
+    { value: "WEDNESDAY", label: "Srijeda" },
+    { value: "THURSDAY", label: "Četvrtak" },
+    { value: "FRIDAY", label: "Petak" },
+    { value: "SATURDAY", label: "Subota" },
+    { value: "SUNDAY", label: "Nedjelja" },
+  ];
+  
