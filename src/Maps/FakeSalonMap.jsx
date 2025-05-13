733397dcc820b3bac2dc7561ed7d238267823276
fejzo9 <fejzo999@gmail.com>
2025-05-13 11:16:17 +0200

    From FakeSalonMap removed SalonsPage component

--- src/Maps/FakeSalonMap.jsx
+++ src/Maps/FakeSalonMap.jsx
@@ -4,7 +4,6 @@ import SalonsPage from '../Salons/SalonsPage';
 function FakeSalonMap(){
     return(
         <div className="mapa">
-            <SalonsPage />
             <h2>Hairdressing Salon Maps</h2>
             <img src="/maps2.png" alt="Maps of Salon" style={{ width: "100%", height: "auto" }}/>
         </div>
